e12b70de8f6683f1211b0de5a31b1bef5207e11b
Vincenzo Acinapura <vin.aci@gmail.com>
2015-10-22 17:44:52 +0200

    Added SPA page

---
+++ javascripts/app.js
@@ -0,0 +1,24 @@
+var renderListItem = function(item, listElementTemplate) {
+  var html = listElementTemplate;
+
+  html = html.replace('_TITLE_', item['title']);
+  html = html.replace('_OVERVIEW_', item['overview'].slice(0, 140).trim() + '&hellip;');
+  html = html.replace('_PICTURE_', item['picture']);
+  html = html.replace('_AVERAGE_STARS_', item['averageStars']);
+  html = html.replace('_YEAR_', item['year']);
+
+  return html;
+}
+
+var renderList = function(items, listElement, listElementTemplate) {
+  listElement.innerHTML = '';
+
+  items.forEach(function(item, i) {
+    listElement.innerHTML += renderListItem(item, listElementTemplate);
+  });
+}
+
+var listElement = document.querySelector('#list');
+var listItemTemplate = document.querySelector('#template-list-item').innerHTML;
+
+renderList(database, listElement, listItemTemplate);
---
+++ javascripts/database.js
@@ -0,0 +1,169 @@
+var database = [
+  {
+    "id": 1,
+    "title": "Game of Thrones",
+    "overview": "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+    "year": 2011,
+    "seasons": 5,
+    "genres": [
+      "Sci-Fi & Fantasy",
+      "Drama"
+    ],
+    "averageStars": 4,
+    "popularity": 8,
+    "airingToday": true,
+    "picture": "https://image.tmdb.org/t/p/w396/jIhL6mlT7AblhbHJgEoiBIOUVl1.jpg",
+    "createdBy": [
+      "David Benioff",
+      "D. B. Weiss"
+    ],
+    "starring": [
+      "Peter Dinklage",
+      "Nikolaj Coster-Waldau",
+      "Lena Headey",
+      "Emilia Clarke",
+      "Kit Harington"
+    ]
+  }, {
+    "id": 2,
+    "title": "Mr. Robot",
+    "overview": "A contemporary and culturally resonant drama about a young programmer, Elliot, who suffers from a debilitating anti-social disorder and decides that he can only connect to people by hacking them. He wields his skills as a weapon to protect the people that he cares about. Elliot will find himself in the intersection between a cybersecurity firm he works for and the underworld organizations that are recruiting him to bring down corporate America.",
+    "year": 2015,
+    "seasons": 1,
+    "genres": [
+      "Drama",
+      "Crime"
+    ],
+    "averageStars": 4,
+    "popularity": 7,
+    "airingToday": false,
+    "picture": "https://image.tmdb.org/t/p/w396/jpV5GWRe0hjZBbx1kPFISvH8QxI.jpg",
+    "createdBy": [
+      "Sam Esmail"
+    ],
+    "starring": [
+      "Rami Malek",
+      "Christian Slater",
+      "Portia Doubleday",
+      "Carly Chaikin",
+      "Martin Wallström"
+    ]
+  }, {
+    "id": 3,
+    "title": "Grey's Anatomy",
+    "overview": "Grey's Anatomy is an American television medical drama that premiered on the American Broadcasting Company as a mid-season replacement on March 27, 2005. The series has aired nine seasons, and focuses on the fictional lives of surgical interns and residents as they gradually evolve into seasoned doctors, while trying to maintain personal lives.",
+    "year": 2005,
+    "seasons": 12,
+    "genres": [
+      "Drama"
+    ],
+    "averageStars": 4,
+    "popularity": 5,
+    "airingToday": true,
+    "picture": "https://image.tmdb.org/t/p/w396/tnkYVbGDmpIcKWo7Synr4yqEsZM.jpg",
+    "createdBy": [
+      "Shonda Rhimes"
+    ],
+    "starring": [
+      "Ellen Pompeo",
+      "Patrick Dempsey",
+      "Justin Chambers",
+      "Chandra Wilson",
+      "James Pickens Jr."
+    ]
+  }, {
+    "id": 4,
+    "title": "Breaking Bad",
+    "overview": "Breaking Bad is an American crime drama television series created and produced by Vince Gilligan. Set and produced in Albuquerque, New Mexico, Breaking Bad is the story of Walter White, a struggling high school chemistry teacher who is diagnosed with inoperable lung cancer at the beginning of the series. He turns to a life of crime, producing and selling methamphetamine, in order to secure his family's financial future before he dies, teaming with his former student, Jesse Pinkman. Heavily serialized, the series is known for positioning its characters in seemingly inextricable corners and has been labeled a contemporary western by its creator.",
+    "year": 2008,
+    "seasons": 5,
+    "genres": [
+      "Drama"
+    ],
+    "averageStars": 4,
+    "popularity": 5,
+    "airingToday": false,
+    "picture": "https://image.tmdb.org/t/p/w396/1yeVJox3rjo2jBKrrihIMj7uoS9.jpg",
+    "createdBy": [
+      "Vince Gilligan"
+    ],
+    "starring": [
+      "Bryan Cranston",
+      "Anna Gunn",
+      "Aaron Paul",
+      "Dean Norris",
+      "Betsy Brandt"
+    ]
+  }, {
+    "id": 5,
+    "title": "Friends",
+    "overview": "Friends is an American sitcom created by David Crane and Marta Kauffman, which aired on NBC from September 22, 1994 to May 6, 2004. The series revolves around a group of friends in the New York City borough of Manhattan. The series was produced by Bright/Kauffman/Crane Productions, in association with Warner Bros. Television. The original executive producers were Crane, Kauffman, and Kevin S. Bright, with numerous others being promoted in later seasons.",
+    "year": 1994,
+    "seasons": 10,
+    "genres": [
+      "Comedy"
+    ],
+    "averageStars": 3.5,
+    "popularity": 5,
+    "airingToday": true,
+    "picture": "https://image.tmdb.org/t/p/w396/7buCWBTpiPrCF5Lt023dSC60rgS.jpg",
+    "createdBy": [
+      "Marta Kauffman",
+      "David Crane"
+    ],
+    "starring": [
+      "Jennifer Aniston",
+      "Courteney Cox",
+      "Lisa Kudrow",
+      "Matt LeBlanc",
+      "Matthew Perry"
+    ]
+  }, {
+    "id": 6,
+    "title": "Sex and the City",
+    "overview": "Sex and the City is an American television romantic sit-com created by Darren Star and produced by HBO. Broadcast from 1998 until 2004, the original run of the show had a total of 94 episodes. Throughout its six-year run, the show received contributions from various producers, writers and directors, perhaps most significantly from Michael Patrick King.",
+    "year": 1998,
+    "seasons": 6,
+    "genres": [
+      "Comedy"
+    ],
+    "averageStars": 4.5,
+    "popularity": 5,
+    "airingToday": true,
+    "picture": "https://image.tmdb.org/t/p/w396/z6IQ6nr2Cz7FUPmQtbVkgHeJ4vK.jpg",
+    "createdBy": [
+      "Darren Star"
+    ],
+    "starring": [
+      "Sarah Jessica Parker",
+      "Kim Cattrall",
+      "Cynthia Nixon",
+      "Kristin Davis"
+    ]
+  }, {
+    "id": 6,
+    "title": "Desperate Housewives",
+    "overview": "Desperate Housewives is an American television comedy-drama-mystery series created by Marc Cherry and produced by ABC Studios and Cherry Productions. It aired Sundays at 9 P.M. Eastern/8 P.M. Central, on ABC from October 3, 2004, until May 13, 2012. Executive producer Cherry served as showrunner. Other executive producers since the fourth season included Bob Daily, George W. Perkins, John Pardee, Joey Murphy, David Grossman, and Larry Shaw.",
+    "year": 2004,
+    "seasons": 8,
+    "genres": [
+      "Mystery",
+      "Drama",
+      "Comedy"
+    ],
+    "averageStars": 4.5,
+    "popularity": 5,
+    "airingToday": false,
+    "picture": "https://image.tmdb.org/t/p/w396/sFqDeJRK4FRIQmwFlOveOLSAT83.jpg",
+    "createdBy": [
+      "Marc Cherry"
+    ],
+    "starring": [
+      "Felicity Huffman",
+      "Eva Longoria",
+      "Teri Hatcher",
+      "Marcia Cross",
+      "Doug Savant"
+    ]
+  }
+];
