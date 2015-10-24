318a00f903266676702ccf7747a38cd2b10f8a1b
Vincenzo Acinapura <vin.aci@gmail.com>
2015-10-24 13:48:15 +0200

    Better code is better

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,26 +1,22 @@
-
-/**
- * renderListItem
- * ---
- * Takes an object and a template string and returns a chunk of HTML code.
- * @param  {[object]} item                An object
- * @param  {[string]} listElementTemplate A template string
- * @return {[string]}                     A chunk of HTML code.
- */
 var renderListItem = function(item, listElementTemplate) {
-  var html = listElementTemplate;
+  var overview = item['overview'].slice(0, 140).trim() + '&hellip;';
+  var genres = item['genres'].map(function(genre) {
+    return '<li>' + genre + '</li>';
+  }).join('');
 
-  html = html.replace('_TITLE_', item['title']);
-  html = html.replace('_OVERVIEW_', item['overview'].slice(0, 140).trim() + '&hellip;');
-  html = html.replace('_PICTURE_', item['picture']);
-  html = html.replace('_AVERAGE_STARS_', item['averageStars']);
-  html = html.replace('_GENRES_', item['genres']);
-  html = html.replace('_YEAR_', item['year']);
-
-  return html;
+  return listElementTemplate
+    .replace('_TITLE_',         item['title'])
+    .replace('_OVERVIEW_',      overview)
+    .replace('_PICTURE_',       item['picture'])
+    .replace('_AVERAGE_STARS_', item['averageStars'])
+    .replace('_GENRES_',        genres)
+    .replace('_YEAR_',          item['year'])
 }
 
-function renderList(items, listElement, listElementTemplate) {
+var renderList = function(items) {
+  var listElement = document.querySelector('#list');
+  var listElementTemplate = document.querySelector('#template-list-item').innerHTML;
+
   listElement.innerHTML = '';
 
   items.forEach(function(item) {
@@ -28,40 +24,32 @@ function renderList(items, listElement, listElementTemplate) {
   });
 }
 
-var listElement = document.querySelector('#list');
-var listElementTemplate = document.querySelector('#template-list-item').innerHTML;
-
 var showItems = function(whatToShow) {
+  var databaseCopy = database.slice();
   var itemsToShow = [];
 
   switch (whatToShow) {
-    case 'all':
-      itemsToShow = database.sort(function(first, second) {
-        return first['year'] < second['year'];
-      });
-      break;
     case 'today':
-      database.forEach(function(item) {
-        if (item['airingToday']) { itemsToShow.push(item); }
+      itemsToShow = databaseCopy.filter(function(item) {
+        return item['airingToday'];
       });
       break;
     case 'popular':
-      itemsToShow = database.sort(function(first, second) {
+      itemsToShow = databaseCopy.sort(function(first, second) {
         return first['popularity'] < second['popularity'];
       });
       break;
     case 'rating':
-      itemsToShow = database.sort(function(first, second) {
+      itemsToShow = databaseCopy.sort(function(first, second) {
         return first['averageStars'] < second['averageStars'];
       });
       break;
+    default:
+      itemsToShow = databaseCopy;
+      break;
   }
 
-  renderList(itemsToShow, listElement, listElementTemplate);
+  renderList(itemsToShow);
 }
 
-// function introduceYourself(name, age) {
-//   return "Ciao, mi chiamo " + name + " ed ho " + age + " anni";
-// }
-//
-// introduceYourself("vincenzo", 34);
+renderList(database);
